98f0ef44a9a450a2c730446adb54fc01e4481f3e
Jarkko Laine <jarkko@jlaine.net>
2009-04-07 14:11:47 +0300

    Forms in ActionPack.

--- library/app/controllers/books_controller.rb
+++ library/app/controllers/books_controller.rb
@@ -8,4 +8,20 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
+  
+  def new
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(params[:book])
+    
+    if @book.save
+      flash[:notice] = "Book created successfully"
+      redirect_to books_path
+    else
+      flash.now[:notice] = "Oops, didn't work"
+      render :action => "new"
+    end
+  end
 end
--- library/app/models/author.rb
+++ library/app/models/author.rb
@@ -1,4 +1,8 @@
 class Author < ActiveRecord::Base
   #belongs_to :book
   has_and_belongs_to_many :books
+  
+  def name
+    "#{first_name} #{last_name}"
+  end
 end
