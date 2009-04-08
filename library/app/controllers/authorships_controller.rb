99d62e40a336b36333d0a0c84c6c4425c12cf4bf
Jarkko Laine <jarkko@jlaine.net>
2009-04-08 09:32:28 +0300

    More authorships goodness.

--- library/app/controllers/authorships_controller.rb
+++ library/app/controllers/authorships_controller.rb
@@ -1,8 +1,28 @@
 class AuthorshipsController < ApplicationController
+  before_filter :fetch_book
+  
+  def new
+    @authors = Author.all
+  end
+  
+  def create
+    @author = Author.find(params[:author_id])
+    @book.authors << @author
+    flash[:notice] = "Successfully added #{@author.name} to the book's authors"
+    redirect_to book_path(@book)
+  end
+  
   def destroy
-    @book = Book.find(params[:book_id])
     @author = @book.authors.find(params[:id])
     @book.authors.destroy(@author)
     redirect_to book_path(@book)
+  end
+  
+  private
+  
+  def fetch_book
+    @book = Book.find(params[:book_id])
+  rescue ActiveRecord::RecordNotFound
+    render :text => "Not found", :status => :not_found
   end
 end
--- library/app/models/book.rb
+++ library/app/models/book.rb
@@ -4,8 +4,4 @@ class Book < ActiveRecord::Base
   
   #has_many :authors
   has_and_belongs_to_many :authors
-  
-  def to_param
-    "#{id}-#{title.gsub(/\s/, "-")}"
-  end
 end
